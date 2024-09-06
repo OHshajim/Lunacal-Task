d24affdf0b100d96bdbff5f5d565ca5b7b307499
OHshajim <ajshajimmax@gmail.com>
2024-09-07 00:12:53 +0600

    gallary finish

--- src/Components/Album.jsx
+++ src/Components/Album.jsx
@@ -8,14 +8,18 @@ const Album = () => {
           </button>
         </div>
         <div className="flex gap-5">
-          <button className="btn bg-[#242424] text-white rounded-2xl">
-            + Add Image
-          </button>
+          <button type="file" className="btn bg-[#242424] text-white rounded-2xl">+ Add Image</button>
+            
           <div className="flex gap-2">
-            <button className="btn rounded-full">+</button>
-            <button className="btn rounded-full">/</button>
+            <button className="btn rounded-full">{`<`}</button>
+            <button className="btn rounded-full">{`>`}</button>
           </div>
         </div>
+      </div>
+      <div className="flex gap-10 mt-16">
+        <img src="/public/Rectangle 5160.png" alt="" className="hover:scale-105 duration-200 delay-75 transform" />
+        <img src="/public/Rectangle 5160.png" alt="" className="hover:scale-105 duration-200 delay-75 transform" />
+        <img src="/public/Rectangle 5160.png" alt="" className="hover:scale-105 duration-200 delay-75 transform" />
       </div>
     </div>
   );
